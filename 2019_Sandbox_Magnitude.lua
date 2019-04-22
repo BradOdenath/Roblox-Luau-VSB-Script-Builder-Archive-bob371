b0ba415dcdafe38db2bc4c04160ba4b766ccf926
Brad Odenath <brad.odenath@gmail.com>
2019-04-22 10:57:56 -0400

    Create 2019_Sandbox_Magnitude.lua

---
+++ 2019_Sandbox_Magnitude.lua
@@ -0,0 +1,75 @@
+print('spongebob colors')
+
+function Part(anc, sz, pa) 
+	local p = Instance.new("Part")
+	p.Locked = anc -- easier to move in testing
+	p.formFactor = "Custom"
+	p.BrickColor = BrickColor.Green()
+	--p.Material = "Plastic"
+	p.TopSurface = 0
+	p.BottomSurface = 0
+	p.CanCollide = true
+	p.Size = sz
+	p.Parent = pa
+	p.Anchored = anc
+	if (anc == true) then
+		local m = Instance.new("BlockMesh")
+		m.Name = "thang"
+		m.Scale = Vector3.new(0.975,1,0.975)
+		m.Parent = p
+	end
+	return p
+end
+
+OFFSET = 20
+
+thing = Part(true, Vector3.new(OFFSET/2,OFFSET/2,OFFSET/2), nil)
+
+function map()
+	local chief = Instance.new("Model")
+	for i = 1, OFFSET*2 do
+		for j = 1, OFFSET*2 do
+			local g = thing:Clone()
+			g.Parent = chief
+			g.CFrame = CFrame.new( (i * OFFSET/2) - (math.pow(OFFSET, 2)/2) , 0, (math.pow(OFFSET, 2)/2) - (j * OFFSET/2) )
+		end
+	end
+	chief.Parent = game.Workspace
+	return chief
+end
+
+function ball() 
+	local b = Part(	false, 
+					Vector3.new(4,4,4),
+					game.Workspace
+	)
+	b.Reflectance = 1
+	b.CFrame = CFrame.new(0,(OFFSET + b.Size.y),0)
+	b.Shape = 2
+	return b
+end
+
+function reflect(map, ball)
+	for i,v in pairs(map:GetChildren()) do
+		local c = (v.Position - ball.Position).magnitude
+		local f = v.CFrame
+		pcall(function()
+			v.Color = Color3.new(0,255-(100/c),0)
+			if ( c > OFFSET) then
+				v.Size = Vector3.new(OFFSET/2, 1+(OFFSET/2*math.sin(math.pi/2*c)), OFFSET/2)
+				v.CFrame = f
+			else
+				v.Size = Vector3.new(OFFSET/2, 1, OFFSET/2)
+			end
+		end)
+	end
+end
+
+function main()
+	local m = map()
+	local b = ball()
+	game:GetService("RunService").Heartbeat:connect(function() reflect(m,b) end)
+end
+
+main()
+
