eb2fea0a274ad2cc47ada190ad3e383667e19e67
Brad Odenath <brad.odenath@gmail.com>
2019-07-08 19:12:02 -0400

    Create 2009_Epic_Path.lua

---
+++ 2009_Epic_Path.lua
@@ -0,0 +1,32 @@
+Player = game.Players.bob371
+Character = Player.Character
+
+Model = Instance.new("Model",Character)
+
+P = Instance.new("Part")
+P.Anchored = true
+P.Locked = true
+P.CanCollide = true
+P.formFactor = "Custom"
+P.Size = Vector3.new(10,0.5,10)
+P.TopSurface = 0
+P.BottomSurface = 0
+M = Instance.new("CylinderMesh",P)
+M.Bevel = 0.1
+
+while true do wait()
+P2 = P:Clone()
+P2.Color = Color3.new(math.random(),math.random(),math.random())
+P2.Parent = Model
+P2.CFrame = Character.Torso.CFrame - Vector3.new(0,3.2,0)
+for i,v in pairs(Model:GetChildren()) do
+if v:IsA("Part") then
+if v.Mesh.Scale.x > 0.01 then
+v.Mesh.Scale = v.Mesh.Scale + Vector3.new(-0.05,0.05,-0.05)
+--v.Mesh.Offset = v.Mesh.Offset - Vector3.new(0,0.1,0)
+else
+v:Remove()
+end
+end
+end
+end
