b1510a2f1d77d826a359ac64da8add2bcb6f1984
Brad Odenath <brad.odenath@gmail.com>
2021-09-20 08:28:23 -0400

    Puppy Morph Updated To VBS

--- 2009_Puppy_Morph.lua
+++ 2009_Puppy_Morph.lua
@@ -1,5 +1,11 @@
+-- Commands
+-- grrrr...
+-- woof
+-- mute 2
+
 scale = 0.5
-player = game.Players.bob371
+player = game.Players.LocalPlayer
+if owner then player = owner end
 z = player.Character 
 for _,l in pairs (z:GetChildren()) do 
 if l.className == "Hat" then 
@@ -33,8 +39,17 @@ rl = z["Right Leg"]
 h = z.Head 
 t = z.Torso 
 sizeup = {t,h,ll,rl,ra,la} 
+
+la.Size = Vector3.new(1,1,1)
+ra.Size = la.Size
+ll.Size = la.Size
+rl.Size = la.Size
+t.Size = Vector3.new(2,1,2)
+t.Transparency = 1
+h.Size = Vector3.new(1,1,1)
+
 for i = 1, #sizeup do 
-sizeup[i].Size = Vector3.new(sizeup[i].Size.x * scale,sizeup[i].Size.y * scale,sizeup[i].Size.z * scale) 
+--sizeup[i].Size = Vector3.new(sizeup[i].Size.x * scale,sizeup[i].Size.y * scale,sizeup[i].Size.z * scale) 
 sizeup[i].TopSurface = 0 
 sizeup[i].BottomSurface = 0 
 sizeup[i].RightSurface = 0 
@@ -54,7 +69,7 @@ ls.Name = "Left Shoulder"
 ls.Part0 = t 
 ls.Part1 = la 
 ls.MaxVelocity = 0.1 
-ls.C0 = CFrame.new(-sizex - la.Size.x/2+0.5,la.Size.y/4-1,-1) * CFrame.fromEulerAnglesXYZ(0,-3.14/2,0) 
+ls.C0 = CFrame.new(-sizex - la.Size.x/2+1,la.Size.y/4-1,-1) * CFrame.fromEulerAnglesXYZ(0,-3.14/2,0) 
 ls.C1 = CFrame.new(0,la.Size.y/4,0) * CFrame.fromEulerAnglesXYZ(0,3.14/2,0) 
 
 rs = Instance.new("Motor") 
@@ -63,7 +78,7 @@ rs.Name = "Right Shoulder"
 rs.Part0 = t 
 rs.Part1 = ra 
 rs.MaxVelocity = 0.1 
-rs.C0 = CFrame.new(sizex + ra.Size.x/2-0.5,ra.Size.y/4-1,-1) * CFrame.fromEulerAnglesXYZ(0,3.14/2,0) 
+rs.C0 = CFrame.new(sizex + ra.Size.x/2-1,ra.Size.y/4-1,-1) * CFrame.fromEulerAnglesXYZ(0,3.14/2,0) 
 rs.C1 = CFrame.new(0,ra.Size.y/4,0) * CFrame.fromEulerAnglesXYZ(0,3.14/2,0) 
 
 lh = Instance.new("Motor") 
@@ -72,7 +87,7 @@ lh.Name = "Left Hip"
 lh.Part0 = t 
 lh.Part1 = ll 
 lh.MaxVelocity = 0.1 
-lh.C0 = CFrame.new(sizex - ll.Size.x*1.5+0.5,-(ll.Size.y/4)*3,0) * CFrame.fromEulerAnglesXYZ(0,-3.14/2,0) 
+lh.C0 = CFrame.new(sizex - ll.Size.x*1.5,-(ll.Size.y/4)*3,0) * CFrame.fromEulerAnglesXYZ(0,-3.14/2,0) 
 lh.C1 = CFrame.new(0,ll.Size.y/4,0) * CFrame.fromEulerAnglesXYZ(0,3.14/2,0) 
 
 rh = Instance.new("Motor") 
@@ -81,7 +96,7 @@ rh.Name = "Right Hip"
 rh.Part0 = t 
 rh.Part1 = rl 
 rh.MaxVelocity = 0.1 
-rh.C0 = CFrame.new(sizex - rl.Size.x/2+0.5,-(ll.Size.y/4)*3,0) * CFrame.fromEulerAnglesXYZ(0,3.14/2,0) 
+rh.C0 = CFrame.new(sizex - rl.Size.x/2,-(ll.Size.y/4)*3,0) * CFrame.fromEulerAnglesXYZ(0,3.14/2,0) 
 rh.C1 = CFrame.new(0,rl.Size.y/4,0) * CFrame.fromEulerAnglesXYZ(0,3.14/2,0) 
 
 
@@ -90,7 +105,7 @@ n.Parent = t
 n.Name = "Neck" 
 n.Part0 = t 
 n.Part1 = h 
-n.C0 = CFrame.new(0,sizey + h.Size.y/2,-1) * CFrame.fromEulerAnglesXYZ(0,0,0) 
+n.C0 = CFrame.new(0,sizey + h.Size.y/2-0.5,-1) * CFrame.fromEulerAnglesXYZ(0,0,0) 
 
 a = z.Animate 
 a:Remove() 
@@ -362,6 +377,7 @@ partz.BottomSurface = 0
 partz.formFactor = "Plate"
 partz.Size = Vector3.new(1,0.4,1)
 partz.BrickColor = z.Torso.BrickColor
+partz.TopSurface, partz.BottomSurface = 0,0
 partz.Transparency = 0
 meshz = Instance.new("SpecialMesh")
 meshz.MeshType = "Brick"
@@ -382,6 +398,7 @@ parta.BottomSurface = 0
 parta.formFactor = "Plate"
 parta.Size = Vector3.new(1,0.4,1)
 parta.BrickColor = z.Torso.BrickColor
+parta.TopSurface, parta.BottomSurface = 0,0
 parta.Transparency = 0
 mesha = Instance.new("SpecialMesh")
 mesha.MeshType = "Brick"
@@ -393,7 +410,6 @@ wz.Part0 = z["Torso"]
 wz.Part1 = parta
 wz.C0 = CFrame.new(0,0.5,0.5) * CFrame.fromEulerAnglesXYZ(-1,0,0)
 
--- Commands
 
 function Chat(msg)
 if msg == "grrrr..." then
@@ -405,16 +421,12 @@ Ex.Parent = game.Workspace
 wait(1)
 Ff:Remove()
 end
---[[if msg == "woof" then
+if msg == "woof" then
 for i,v in pairs(game.Players:GetChildren()) do
 if v.className == "Player" then
-if v.Character:findFirstChild("Humanoid") ~= nil then
-if (v.Character.Torso.Position - z.Torso.Position).magnitude < 15 then
-if v.Name ~= vip.Name then
+pcall(function()
 v.Character.Humanoid.Jump = true
-end
-end
-end
+end)
 end
 end
 wait(0.1)
@@ -429,8 +441,8 @@ end
 end
 end
 end
-end]]
-if msg == "/sc 1" then
+end
+if msg == "/mute 2" then
 partz = Instance.new("Part")
 partz.Name = "TemporaryTorso"
 partz.CanCollide = true
@@ -477,7 +489,7 @@ player.Chatted:connect(Chat)
 -- Hats
 
 for _,l in pairs (game.Lighting:GetChildren()) do 
-if l.className == "Hat" then 
+if l.className == "Accesory" then 
 l.Parent = z
 end end
 
@@ -487,6 +499,7 @@ z.Humanoid.WalkSpeed = 18
 
 -- Wag
 
+coroutine.resume(coroutine.create(function()
 while true do wait()
 wz.C0 = CFrame.new(0.05,0.5,0.5) * CFrame.fromEulerAnglesXYZ(-1,0.05,0)
 wait()
@@ -496,3 +509,4 @@ wz.C0 = CFrame.new(-0.05,0.5,0.5) * CFrame.fromEulerAnglesXYZ(-1,-0.05,0)
 wait()
 wz.C0 = CFrame.new(0,0.5,0.5) * CFrame.fromEulerAnglesXYZ(-1,0,0)
 end
+end))
