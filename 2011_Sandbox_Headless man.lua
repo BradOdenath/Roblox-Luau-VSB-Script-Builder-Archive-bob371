31cbcfb018c18190e78929bbecba55a94734a710
Brad Odenath <brad.odenath@gmail.com>
2021-09-21 08:39:21 -0400

    Headless Man Updated To VBS

--- 2011_Sandbox_Headless man.lua
+++ 2011_Sandbox_Headless man.lua
@@ -1,44 +1,33 @@
-Player = game.Players.LocalPlayer
+Player = owner or game.Players.LocalPlayer
 Character = Player.Character
-
-game.Workspace.CurrentCamera.CameraSubject = Character.Torso
-game.Workspace.CurrentCamera.CameraType = "Follow"
 
 --[[ * Neck Weld * ]]--
 
-Character.Head.Size = Character.Torso.Size
-Neck = Instance.new("Weld") 
-Neck.Parent = Character["Torso"]
-Neck.Part0 = Neck.Parent
-Neck.Part1 = Character["Head"]
-Neck.C0 = CFrame.new(0,100,0) * CFrame.fromEulerAnglesXYZ(0,0,0)
-Character.Head.Transparency = 1
-pcall(function() Character.Head.face:Remove() end)
-pcall(function() Character.Head.Mesh.Scale = Vector3.new(0,0,0) end)
-
-function Dead()
-Character.Parent = game:getService("Lighting")
-wait()
-Character.Parent = game:getService("Workspace")
-Character:MakeJoints()
-Character.Head.Size = Character.Torso.Size
-Neck = Instance.new("Weld") 
-Neck.Parent = Character["Torso"]
-Neck.Part0 = Neck.Parent
-Neck.Part1 = Character["Head"]
-Neck.C0 = CFrame.new(0,100,0) * CFrame.fromEulerAnglesXYZ(0,0,0)
-Character.Head.Transparency = 1
-pcall(function() Character.Head.face:Remove() end)
-pcall(function() Character.Head.Mesh.Scale = Vector3.new(0,0,0) end)
-pcall(function() Character.Head.Mesh.Scale = Vector3.new(0,0,0) end)
+function NW()
+	Character.Head.Size = Character.Torso.Size
+	Neck = Instance.new("Weld") 
+	Neck.Parent = Character["Torso"]
+	Neck.Part0 = Neck.Parent
+	Neck.Part1 = Character["Head"]
+	Neck.C0 = CFrame.new(0,0.25,0) * CFrame.fromEulerAnglesXYZ(0,0,0)
+	Character.Head.Transparency = 1
+	Character.Head.CastShadow = false
+	pcall(function() Character.Head.face:Remove() end)
+	pcall(function() Character.Head.Mesh.Scale = Vector3.new(0,0,0) end)
 end
 
-Character.Humanoid.Died:connect(Dead)
+function main()
+	game.Workspace.CurrentCamera.CameraSubject = Character.Torso
+	game.Workspace.CurrentCamera.CameraType = "Follow"
+	NW()
+	Character.Humanoid.Died:connect(NW)
+	while true do wait()
+		for i,v in pairs(Character:GetChildren()) do
+			if v:IsA("Hat") then v.Parent = game.Workspace end
+			Character.Head.Transparency = 1
+			pcall(function() Character.Head.Mesh.Scale = Vector3.new(0,0,0) end)
+		end
+	end
+end
 
-while true do wait()
-for i,v in pairs(Character:GetChildren()) do
-if v:IsA("Hat") then v.Parent = game.Workspace end
-Character.Head.Transparency = 1
-pcall(function() Character.Head.Mesh.Scale = Vector3.new(0,0,0) end)
-end
-end
+main()
