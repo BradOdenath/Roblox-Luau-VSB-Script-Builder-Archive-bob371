35bc489f01702890220daaa96bc1c5b98dd70ec2
Brad Odenath <brad.odenath@gmail.com>
2021-09-21 08:01:24 -0400

    Updated CharMapGui To VBS

--- 2010_CharMapGui.lua
+++ 2010_CharMapGui.lua
@@ -1,4 +1,4 @@
-vip = game.Players.bob371
+vip = owner or game.Players.LocalPlayer
 gui = Instance.new("ScreenGui")
 gui.Name = "Character Map"
 gui.Parent = vip.PlayerGui
@@ -11,8 +11,6 @@ stage = 0
 
 space = 100
 
-charn = 0
-
 chars = 256
 
 hold = Instance.new("Frame")
@@ -20,17 +18,16 @@ hold.Name = "Stuff"
 hold.Position = UDim2.new(0,-50,0,0)
 hold.Parent = gui
 
-for i = 1,chars do
+for i = 0,chars do
 ----
 number = number + 1
 if number == 13 then number = 1 stage = stage + 1 end
 buttona = Instance.new("TextLabel")
-buttona.Text = string.char(charn).." = string.char("..charn..")"
+buttona.Text = string.char(i).." = string.char("..i..")"
 buttona.BackgroundTransparency = 0.75
-buttona.Size = UDim2.new(0,100,0,25)
-buttona.Position = UDim2.new(0,100*number,0,space+(stage*25))
+buttona.Size = UDim2.new(0,140,0,25)
+buttona.Position = UDim2.new(0,140*number,0,space+(stage*25))
 buttona.Parent = hold
 wait()
-charn = charn + 1
 ----
 end
