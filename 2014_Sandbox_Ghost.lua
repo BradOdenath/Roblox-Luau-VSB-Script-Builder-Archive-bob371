ffaa06a1dfbda5030f440cedd2dbb3a9f19367f4
Brad Odenath <brad.odenath@gmail.com>
2021-09-29 00:27:34 -0400

    Ghost 2014 Updated To VBS

--- 2014_Sandbox_Ghost.lua
+++ 2014_Sandbox_Ghost.lua
@@ -1,49 +1,48 @@
-Player = game:getService(“Players”).bob371
+Player = owner or game:getService("Players").bob371
 Character = Player.Character
 
 Model = Instance.new("Model",Character)
-Model.Name = ""
+Model.Name = Character.Name
 
 Human = Instance.new("Humanoid",Model)
 Human.MaxHealth = 0
 
-pcall(function() Character.Shirt:Remove() end)
-pcall(function() Character.Pants:Remove() end)
-
+function gpart(part)
+	local p = Instance.new("Part")
+	p.CanCollide = false
+	p.Locked = true
+	p.Anchored = true
+	p.Massless = true
+	p.CastShadow = false
+	p.Size = part.Size
+	local pc = part:GetChildren()
+	if pc then
+		for i,v in pairs(pc) do
+			if v:IsA("SpecialMesh") or v:IsA("BlockMesh") then
+				v:Clone().Parent = p
+			end
+		end
+	end
+	p.Parent = Model
+	p.CFrame = part.CFrame
+	return p
+end
 
 while true do wait(0.1)
-for i,v in pairs(Character:GetChildren()) do
-if v:IsA("Shirt") then v:Clone().Parent = Model end
-if v:IsA("Pants") then v:Clone().Parent = Model end
-if v:IsA("Part") then
-a = v:Clone()
-a.Anchored = true
-a.Locked = true
-a.CanCollide = false
-a.Parent = Model
-a.Name = "Part"
-a.CFrame = v.CFrame
-end
-if v:IsA("Hat") then
-pcall(function()
-a = v.Handle:Clone()
-a.Anchored = true
-a.Locked = true
-a.CanCollide = false
-a.Parent = Model
-a.CFrame = v.Handle.CFrame
-end)
-end
-end
+	for i,v in pairs(Character:GetChildren()) do
+		if v:IsA("Part") then
+			gpart(v)
+		end
+	end
 
-for i,v in pairs(Model:GetChildren()) do
-if v:IsA("Part") then
-if v.Transparency < 1 then
-v.Transparency = v.Transparency + 0.1
-else
-v:Remove()
-end
-end
-end
+	for i,v in pairs(Model:GetChildren()) do
+		if v:IsA("Part") then
+			if v.Transparency < 1 then
+				v.Transparency = v.Transparency + 0.1
+			else
+				v:Remove()
+			end
+		end
+	end
 
 end
