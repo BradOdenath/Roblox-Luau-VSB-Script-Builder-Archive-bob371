47a4e88b5f0b891f4c3d79937a6d041fd17943f6
Brad Odenath <brad.odenath@gmail.com>
2021-10-17 17:18:15 -0400

    Updated To VSB

--- 2021_VSB_Suit_R6_R15_Penguin.lua
+++ 2021_VSB_Suit_R6_R15_Penguin.lua
@@ -22,6 +22,97 @@ local v3n, cfn, cfa, cfe, c3 = Vector3.new, CFrame.new, CFrame.Angles, CFrame.fr
 local pi = math.pi
 
 --[[
+	Framework: Roblox
+	Language: Lua
+	Project: LuigiFan's Roblox Iron Man R6/R15 (2021)
+	Coders: supergod800, bob371
+	Designers: LuigiFan
+]]--
+
+local Keys,downs,lastpressed={},{},{}  --Shoutout to @supergod800
+
+local function isKeyDown(key) return downs[tostring(key)] or false end
+
+local function emptyFunction(key, key_event) --print("Empty Key Function: "..tostring(key)..key_event) 
+end
+
+local function _setKey(key, func_keydown, func_keyup) 
+	
+	local ku, kd
+	
+	ku = func_keyup
+	
+	kd = func_keydown
+	
+	Keys[key]= {
+		up = ku, 
+		down = kd
+	} 
+	
+end
+
+local function setKey(key,func_keydown, func_keyup) 
+	if typeof(key) == 'table' then 
+		for i,v in pairs(key) do 
+			_setKey(v, func_keydown, func_keyup)
+		end 
+	else 
+		_setKey(key, func_keydown, func_keyup)
+	end
+end
+
+local function timePassed(key)
+    local t = tick() return math.max(t - (lastpressed[key] or t),0)
+end
+
+local function keyDown(plr,key)	
+	key = tostring(key)
+	--print(key)
+	if isKeyDown(key) then 
+		downs[key]=false
+		--print('up')
+		if Keys[key] then 
+			--pcall(function() 
+				Keys[key].up() 
+			--end) 
+		end
+	else
+		downs[key],lastpressed[key]=true,tick()
+		--print('down')
+		if Keys[key] then 
+			--pcall(function() 
+				Keys[key].down() 
+			--end) 
+		end
+	end
+end
+
+local mouse = {}
+local currentcamera = {}
+local keysEvent = Instance.new("RemoteEvent",NLS(string.format([[
+    local keysEvent,mouse = script:FindFirstChildWhichIsA("RemoteEvent"),game.Players.LocalPlayer:GetMouse()
+    local mousedata
+				--, cameradata 
+		= keysEvent:FindFirstChildWhichIsA("RemoteEvent"), keysEvent:FindFirstChildWhichIsA("RemoteEvent")
+    mouse.KeyDown:connect(function(plr,key)  keysEvent:FireServer(plr,key) end)
+    mouse.KeyUp:connect(function(plr,key) keysEvent:FireServer(plr,key) end)
+    mouse.Button1Down:connect(function(plr,key) keysEvent:FireServer(plr,'MouseButton1Down') end)
+    mouse.Button1Up:connect(function(plr,key) keysEvent:FireServer(plr,'MouseButton1Down') end)
+    local runserv = game:GetService("RunService")
+    while runserv.Stepped:Wait() do
+        mousedata:FireServer(plr,{Hit = mouse.Hit,Target = mouse.Target, TargetSurface = mouse.TargetSurface})
+		--cameradata:FireServer(plr,{Cam = workspace.CurrentCamera})
+    end
+    ]],''),
+player.PlayerGui))
+	
+local mouseEvent = Instance.new('RemoteEvent',keysEvent)
+--local cameraEvent = Instance.new('RemoteEvent',keysEvent)
+mouseEvent.OnServerEvent:Connect(function(plr,data) mouse = data end)
+--cameraEvent.onServerEvent:Connect(function(plr,data) currentcamera = data end)
+keysEvent.OnServerEvent:Connect(keyDown)
+
+--[[
 local face = nil
 local migrateFace = function(_c)
 	local _head = _c:FindFirstChild("Head")
@@ -144,7 +235,7 @@ local set_transparency = function(model, transparency)
 	end
 end
 
-local invisiblify = function(model)
+local invisibilify = function(model)
 	set_transparency(model, 1)
 end
 
@@ -163,9 +254,12 @@ local visiblify = function(model)
 	end
 end
 
+local weldTagName = "HEEEEEEEYYYAAAAAAA"
+
 local Weld = function(part0,part1,c0)
 	local _w = Instance.new("Weld")
-	_w.Parent = part1
+	_w.Name = weldTagName
+	_w.Parent = part0
 	_w.Part0 = part0
 	_w.Part1 = part1
 	_w.C0 = c0
@@ -214,10 +308,10 @@ local findFirstArrayDataByNameTag = function(arr, tag)
 		end
 	end
 end
+local LArm, RArm
 
-local LArm, RArm, 
-	LCore, RCore,
-	MCore
+local 	coreL, coreR,
+	coreO
 local ArmorAddons = function(_characterArmorData)
 	local _armorAddons = {}
 	
@@ -239,7 +333,40 @@ local ArmorAddons = function(_characterArmorData)
 	}
 	_core = WeldMeshPart(WeldPart(_core.name, _core.size, _core.color, _core.shape, _core.reflectance, Core, cfn(0,0,-0.8)),_core.mesh)
 	table.insert(_armorAddons,_core)
-	MCore = _core
+	coreO = _core
+		
+	local _abs = {
+		name = 'Left_Arm_abs',
+		size = v3n(1,1,1) * 0.6,
+		color = COLOR_DEFAULT_SUIT,
+		shape = "Block",
+		reflectance = 0.1,
+		mesh = Mesh("Brick",v3n(1,1,1),v3n(0,0,0))
+	}
+	_abs = WeldMeshPart(WeldPart(_abs.name, _abs.size, _abs.color, _abs.shape, _abs.reflectance, Core, cfn(0,-0.4,-0.8) * cfa(0,0,0)),_abs.mesh)
+	table.insert(_armorAddons,_abs)
+		
+	local _left_chest = {
+		name = 'Left_Arm_left_chest',
+		size = v3n(1,1,1) * 0.6,
+		color = COLOR_DEFAULT_SUIT,
+		shape = "Block",
+		reflectance = 0.1,
+		mesh = Mesh("Brick",v3n(1,1,1),v3n(0,0,0))
+	}
+	_left_chest = WeldMeshPart(WeldPart(_left_chest.name, _left_chest.size, _left_chest.color, _left_chest.shape, _left_chest.reflectance, Core, cfn(-0.3,0.1,-0.8) * cfa(0,0,-math.pi/12)),_left_chest.mesh)
+	table.insert(_armorAddons,_left_chest)
+		
+	local _right_chest = {
+		name = 'Left_Arm_right_chest',
+		size = v3n(1,1,1) * 0.6,
+		color = COLOR_DEFAULT_SUIT,
+		shape = "Block",
+		reflectance = 0.1,
+		mesh = Mesh("Brick",v3n(1,1,1),v3n(0,0,0))
+	}
+	_right_chest = WeldMeshPart(WeldPart(_right_chest.name, _right_chest.size, _right_chest.color, _right_chest.shape, _right_chest.reflectance, Core, cfn(0.3,0.1,-0.8) * cfa(0,0,math.pi/12)),_right_chest.mesh)
+	table.insert(_armorAddons,_right_chest)
 		
 	local _left_arm_core = {
 		name = 'Left_Arm_Core',
@@ -251,7 +378,7 @@ local ArmorAddons = function(_characterArmorData)
 	}
 	_left_arm_core = WeldMeshPart(WeldPart(_left_arm_core.name, _left_arm_core.size, _left_arm_core.color, _left_arm_core.shape, _left_arm_core.reflectance, LArm, cfn(0,-0.4,0)),_left_arm_core.mesh)
 	table.insert(_armorAddons,_left_arm_core)
-	LCore = _left_arm_core
+	coreL = _left_arm_core
 	
 	local _right_arm_core = {
 		name = 'Right_Arm_core',
@@ -263,7 +390,7 @@ local ArmorAddons = function(_characterArmorData)
 	}
 	_right_arm_core = WeldMeshPart(WeldPart(_right_arm_core.name, _right_arm_core.size, _right_arm_core.color, _right_arm_core.shape, _right_arm_core.reflectance, RArm, cfn(0,-0.4,0)),_right_arm_core.mesh)
 	table.insert(_armorAddons,_right_arm_core)
-	RCore = _right_arm_core
+	coreR = _right_arm_core
 
 	print(_left_arm_core.Parent,_right_arm_core.Parent)
 	return _armorAddons
@@ -305,7 +432,7 @@ local ArmorPart = function(part)
 	return _p
 end
 
-FadeInvisiblifyCharacter = function(c)
+FadeinvisibilifyCharacter = function(c)
 	for j = 0, 1, 0.1 do wait()
 		set_transparency(c, j)
 	end
@@ -456,6 +583,7 @@ local CharacterArmor = function(_c)
 	}
 	_left_arm = WeldMeshPart(WeldPart(_left_arm.name, _left_arm.size, _left_arm.color, _left_arm.shape, _left_arm.reflectance, HumanoidRootPart, cfn(-1.3,-2-_height,0)*cfe(0,math.pi/2,0)),_left_arm.mesh)
 	table.insert(_characterArmor,_left_arm)
+	LArm = _left_arm
 	
 	local _right_arm = {
 		name = 'Right_Arm',
@@ -467,6 +595,7 @@ local CharacterArmor = function(_c)
 	}
 	_right_arm = WeldMeshPart(WeldPart(_right_arm.name, _right_arm.size, _right_arm.color, _right_arm.shape, _right_arm.reflectance, HumanoidRootPart, cfn(1.3,-2-_height,0)*cfe(0,-math.pi/2,0)),_right_arm.mesh)
 	table.insert(_characterArmor,_right_arm)
+	RArm = _right_arm
 	
 	Weld(RightArm,_right_arm,cfn(0,-0.4,0)*cfa(0,0,math.pi/4))
 	Weld(LeftArm,_left_arm,cfn(0,-0.4,0)*cfa(0,0,-math.pi/4))
@@ -519,27 +648,274 @@ ClearSuit = function(_c)
 	for i,v in pairs(_c:GetDescendants()) do
 		if (string.find(v.Name, 'WP') ~= nil) then
 			pcall(function() v:Remove() end)
+		elseif v.Name == weldTagName then
+			v.Part0 = nil
+			v:Remove()
 		end
 	end
 	visiblify(_c)
 end
 
-MainCharacter = function()
-	local _c = character
+local cad
+MainCharacter = function(_c)
 	ClearSuit(_c)
 	pcall(function()
 		_c.Head.face:Remove()
 	end)
-	FadeInvisiblifyCharacter(_c)
-	--migrateFace(_c)
-	local _characterArmorData = CharacterArmor(_c)
-	--wait(10)
-	--ArmorSuit(_c, _characterArmorData)
-	--ClearSuit(_c)
+	invisibilify(_c)
+	--FadeinvisibilifyCharacter(_c)
+	cad = CharacterArmor(_c)
+end
+
+MainIronMan = function(_c)
+	if (cad) then
+		ArmorSuit(_c, cad)
+	end
 end
 
 Main = function()
-	MainCharacter()
+	MainCharacter(character)
 end
 
 Main()
+
+local pointArm = function(Arm, Weld, EndCf)
+	if cad == nil then return end
+	local UpperTorso = findFirstArrayDataByNameTag(cad,'Core')
+	local Torso = findFirstArrayDataByNameTag(cad,'Core')
+	if (Arm:lower():match("right")) then
+		--print(Arm, Weld, EndCf)
+		Weld.C0 = cfn(-1.3,-2-_height,0)*cfe(0,math.pi/2,0)
+		Weld.C1 = cfa(math.rad(-90),0,0) * cfn(0,0,0.5)
+		if (isR15(character)) then
+			Weld.C1 = Weld.C1 * (cfn((UpperTorso.CFrame * cfn(1.5,0,0)).p, EndCf.p) - (UpperTorso.CFrame * cfn(1.5,0,0)).p):inverse()
+			Weld.C1 = Weld.C1 * (UpperTorso.CFrame - UpperTorso.CFrame.p)
+		else
+			Weld.C1 = Weld.C1 * (cfn((Torso.CFrame * cfn(1.5,0,0)).p, EndCf.p) - (Torso.CFrame * cfn(1.5,0,0)).p):inverse()
+			Weld.C1 = Weld.C1 * (Torso.CFrame - Torso.CFrame.p)
+		end
+	elseif (Arm:lower():match("left")) then
+		--print(Arm, Weld, EndCf)
+		Weld.C0 =  cfn(1.3,-2-_height,0)*cfe(0,-math.pi/2,0)
+		Weld.C1 = cfa(math.rad(-90),0,0) * cfn(0,0,0.5)
+		if (isR15(character)) then
+			Weld.C1 = Weld.C1 * (cfn((UpperTorso.CFrame * cfn(-1.5,0,0)).p, EndCf.p) - (UpperTorso.CFrame * cfn(-1.5,0,0)).p):inverse()
+			Weld.C1 = Weld.C1 * (UpperTorso.CFrame - UpperTorso.CFrame.p)
+		else
+			Weld.C1 = Weld.C1 * (cfn((Torso.CFrame * cfn(-1.5,0,0)).p, EndCf.p) - (Torso.CFrame * cfn(-1.5,0,0)).p):inverse()
+			Weld.C1 = Weld.C1 * (Torso.CFrame - Torso.CFrame.p)
+		end
+	end
+end
+
+local pointArms = function()
+	if (LArm ~= nil and RArm ~= nil) then
+		print(LArm, RArm)
+		local LeftArmWeld = LArm:FindFirstChild(weldTagName)
+		local RightArmWeld = RArm:FindFirstChild(weldTagName)
+		pointArm("Left", LeftArmWeld, mouse.Hit)
+		pointArm("right", RightArmWeld, mouse.Hit)
+	end
+end
+
+local ReconfigureArms = function()
+	if (LArm ~= nil and RArm ~= nil) then
+		local LeftArmWeld = LArm:FindFirstChild(weldTagName)
+		local RightArmWeld = RArm:FindFirstChild(weldTagName)
+		LeftArmWeld.C0 = cfn(-1.3,-2-_height,0)*cfe(0,math.pi/2,0)
+		RightArmweld.C0 = cfn(1.3,-2-_height,0)*cfe(0,-math.pi/2,0)
+	end
+end
+
+--[[ * Juice * ]]--
+
+local Juice = Instance.new("BodyPosition")
+Juice.MaxForce = Vector3.new(math.huge,math.huge,math.huge)
+local Swush = Instance.new("BodyGyro")
+Swush.MaxTorque = Vector3.new(math.huge,math.huge,math.huge)
+--Swush.P = math.huge
+--Swush.D = 0
+local Boost = Instance.new("BodyVelocity")
+Boost.MaxForce = Vector3.new(math.huge,math.huge,math.huge)
+
+local HumanoidRootPart = character:FindFirstChild("HumanoidRootPart")
+
+local suit = false
+local flying = false
+local holding = false
+local arm_point = false
+
+local offset = Vector3.new(0,12,0)
+
+local laser = function(place0)
+	coroutine.resume(coroutine.create(function()
+		--print('laser'..tostring(place0))
+		if (mouse.Target ~= nil) then
+			local Place0 = place0.CFrame
+			local Place1 = mouse.Hit.p
+			P = Instance.new("Part")
+			P.Size = Vector3.new(1,1,(Place0.p - Place1).magnitude)
+			P.Name = "Laser"
+			P.Reflectance = 1
+			P.Anchored = true
+			P.BottomSurface, P.TopSurface = 0, 0
+			P.CanCollide = false
+			P.CFrame = CFrame.new((Place0.p + Place1)/2,Place0.p)
+			P.Parent = workspace
+			coroutine.resume(coroutine.create(function()
+				pcall(function()
+					while (P) do wait()
+						Place0 = place0.CFrame
+						P.Size = Vector3.new(1,1,(Place0.p - Place1).magnitude)
+						P.CFrame = CFrame.new((Place0.p + Place1)/2,Place0.p)
+					end
+				end)
+				P:Remove()
+			end))
+			local E = Instance.new("Explosion")
+			E.BlastRadius, E.BlastPressure = 10, 1e+006
+			E.Hit:Connect(function(part, distance)
+				if (distance < 10) then
+					part:BreakJoints()
+					part.Anchored = false
+					Instance.new("Fire",part)
+				end
+			end)
+			E.Position = Place1
+			E.Parent = workspace
+			local S = Instance.new("Sound")
+			S.Volume = 100
+			S.Pitch = 1
+			S.SoundId = "rbxasset://sounds\\Launching rocket.wav"
+			S.Parent = _core
+			S:Play()
+			wait(0.1)
+			P.Parent = nil
+			P:Remove()
+			coroutine.resume(coroutine.create(function()
+				wait(10)
+				S:Remove()
+				P.Transparency = 0.5
+			end))
+			
+		end
+	end))
+end
+
+local whileFlying = function()
+	coroutine.resume(coroutine.create(function()
+		while (flying) do wait()
+			Swush.CFrame = CFrame.new(HumanoidRootPart.Position, mouse.Hit.p)
+		end
+	end))
+end
+
+setKey('MouseButton1Down',
+function()
+	holding = true
+	if (flying) then
+		Swush.Parent = HumanoidRootPart
+		Juice.Parent = HumanoidRootPart
+		while ((holding) and (flying)) do wait()
+			if ((mouse.Target ~= nil) and (flying)) then
+				Boost.Parent = nil
+				Juice.Parent = HumanoidRootPart
+				Juice.Position = mouse.Hit.p + offset
+			else
+				Boost.Parent = HumanoidRootPart
+				Boost.Velocity = Swush.CFrame.lookVector * 100
+				Juice.Parent = nil
+			end
+		end
+		Juice.Position = HumanoidRootPart.Position
+		Juice.Parent = HumanoidRootPart
+		Boost.Parent = nil
+--[[	else
+		arm_point = true
+		while (holding) do wait()
+			pointArms()
+		end
+		arm_point = false
+		ReconfigureArms()]]
+	end
+end,
+function()
+	holding = false
+end)
+
+local switchFlying = function()
+	if flying then
+		character.Humanoid.PlatformStand = false
+		flying = false
+		holding = false
+		Swush.Parent = nil
+		Juice.Parent = nil
+		Boost.Parent = nil
+	else
+		character.Humanoid.PlatformStand = true
+		flying = true
+		Swush.Parent = HumanoidRootPart
+		whileFlying()
+		Juice.Parent = HumanoidRootPart	
+		Juice.Position = HumanoidRootPart.Position + offset
+	end
+end
+
+setKey('e',
+	function()
+		switchFlying()
+	end,
+	function()
+	
+	end
+)
+
+setKey('f',
+	function()
+		if (suit) then
+			if (flying) then
+				--if (arm_point) then
+					--laser(coreL)
+					--laser(coreR)
+				--else
+					laser(coreO)
+				--end
+			elseif (holding) then
+				--if (arm_point) then
+					laser(coreL)
+					laser(coreR)
+				--end
+			end
+		end
+	end,
+	function() end)
+
+setKey('q',
+function()
+	if (cad ~= nil) then
+		if (flying == false) then
+			if (suit == false) then
+				MainIronMan(character)
+			else
+				MainCharacter(character)
+			end
+			suit = not suit
+		end
+	end
+end,
+function()
+end)
+
+setKey('r',
+function()
+	Instance.new("ForceField", character)
+end,
+function()
+	for i,v in pairs(character:GetChildren()) do
+		if v:IsA("ForceField") then
+			pcall(function() v:Remove() end)
+		end
+	end
+	ff = nil
+end)
+
